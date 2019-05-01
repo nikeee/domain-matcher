0637edb7c670b84a912444c227c5f354291754ed
Niklas Mollenhauer <nikeee@outlook.com>
2019-05-01 23:40:36 +0200

    Rewrite app in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import React, { PureComponent } from "react";
+import React, { PureComponent, ChangeEvent } from "react";
 
 import "./App.css";
-import { fetchTLDs } from "./tlds";
+import { fetchTLDs, getPossbileTLDs, TLD } from "./tlds";
 
 
 interface StateTLDsLoaded {
-	tlds: string[];
+	tlds: TLD[];
+	desiredNames: string[];
+	possibleDomains: Record<string, string[]>;
 }
 interface StateInitial {
 	tlds: undefined;
+	desiredNames: string[];
+	possibleDomains: undefined;
 }
 
 type State = StateTLDsLoaded | StateInitial;
@@ -21,19 +25,81 @@ export default class App extends PureComponent<Props, State> {
 
 		this.state = {
 			tlds: undefined,
+			desiredNames: [],
+			possibleDomains: undefined,
 		};
 
 		fetchTLDs()
 			.then(tlds => this.setState({
 				tlds,
+				possibleDomains: undefined,
 			}));
+	}
+
+	onChangeDesiredNames = (ev: ChangeEvent<HTMLTextAreaElement>) => {
+		const value = ev.target.value.trim();
+		const s = this.state;
+
+		if (!value || s.tlds === undefined) {
+			return this.setState({
+				desiredNames: [],
+				possibleDomains: undefined,
+			});
+		}
+		const desiredNames = value.toLowerCase()
+			.split("\n")
+			.filter(f => !!f && !!f.trim());
+
+		if(desiredNames.length === 0) {
+			return this.setState({
+				desiredNames: [],
+				possibleDomains: undefined,
+			});
+		}
+
+		const possibleDomains: Record<string, string[]> = {};
+		for (const name of desiredNames)
+			possibleDomains[name] = getPossbileTLDs(s.tlds, name);
+
+		this.setState({
+			desiredNames,
+			possibleDomains,
+		});
 	}
 
 	render() {
 		const s = this.state;
-		// const p = this.props;
-		return s.tlds !== undefined
-			? <h1>Got {s.tlds.length} TLDs</h1>
-			: <h1>Loading TLDs</h1>
+		if (s.tlds === undefined)
+			return (<h1>Loading TLDs...</h1>);
+
+		let possibleList;
+		if (s.possibleDomains === undefined) {
+			possibleList = <h2>Type some string</h2>;
+		} else {
+			const possible = Object.values(s.possibleDomains).flat();
+			possibleList = (
+				<div className="card">
+					<div className="card-header">Possbile Domains</div>
+					<ul className="list-group list-group-flush">
+						{possible.map(d => (<li key={d} className="list-group-item"><code>{d}</code></li>))}
+					</ul>
+				</div>
+			);
+		}
+
+		return (
+			<main>
+				<h1>Got {s.tlds.length} TLDs</h1>
+				<div className="card">
+					<div className="card-header">
+						<label htmlFor="desiredNames">Desired Names:</label>
+					</div>
+					<div className="card-body form-group">
+						<textarea id="desiredNames" className="form-control" rows={10} onChange={this.onChangeDesiredNames} />
+					</div>
+				</div>
+				{possibleList}
+			</main>
+		);
 	}
 }
--- src/tlds.ts
+++ src/tlds.ts
@@ -1,9 +1,18 @@
 const DOMAINS_FILE = "domains.txt";
 
-export async function fetchTLDs(): Promise<string[]> {
+export type TLD = string;
+
+export async function fetchTLDs(): Promise<TLD[]> {
 	const domainsUpper = await fetch(DOMAINS_FILE).then(r => r.text());
 	return domainsUpper
 		.toLowerCase()
 		.split("\n")
 		.filter(line => line.trim() !== "" && !line.trim().startsWith("#"));
+}
+
+export function getPossbileTLDs(tlds: TLD[], desired: string) {
+	const possible = tlds.filter(tld => desired.endsWith(tld));
+	return possible.length === 0
+		? [`No match for <${desired}>`]
+		: possible.map(tld => desired.slice(0, -tld.length) + "." + tld);
 }
